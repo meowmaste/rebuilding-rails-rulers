fe44b9cff5fa616c842f80d8161734642429aa8b
Joslyn <jrosbrook@gmail.com>
2014-02-23 10:34:41 -0800

    ch 4 ex 1

--- test/test_application.rb
+++ test/test_application.rb
@@ -1,18 +1,30 @@
 require_relative "test_helper"
 
+classTestController < Rulers::Controller 
+  def index
+    "Hello!" # Not rendering a view
+  end 
+end 
+
 class TestApp < Rulers::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end 
 end
 
 class RulersAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
+
   def app
     TestApp.new
   end
  
   def test_request
-    get "/"
+    get "/example/route"
+
     assert last_response.ok?
     body = last_response.body
     assert body["Hello"]
   end
+  
 end
