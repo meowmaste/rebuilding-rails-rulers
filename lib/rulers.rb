cd1d75555dad839d801456f0f3e1859dbcd9f1f4
Joslyn <jrosbrook@gmail.com>
2014-02-28 21:48:03 -0800

    chapter6 responses with rack

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,23 +8,22 @@ require "rulers/file_model"
 
 module Rulers
   class Application
-    def call(env)
+    def call(env) # Redefine
       if env['PATH_INFO'] == '/favicon.ico'
         return [404,
           {'Content-Type' => 'text/html'}, []]
-      elsif env['PATH_INFO'] == "/"
-        env['PATH_INFO'] = "/quotes/a_quote"
-      end 
+      end
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      begin 
-        text = controller.send(act)
-      rescue 
-        text = "There was an error."
+      text = controller.send(act)
+      if controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
+      else 
+        [200,
+          {'Content-Type' => 'text/html'}, [text]]
       end 
-      [200, {'Content-Type' => 'text/html'},
-          [text]]
     end
   end 
 end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -32,6 +32,20 @@ module Rulers
       @request ||= Rack::Request.new(@env)
     end 
 
+    def response(text, status=200, headers={})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response # Only for Rulers
+      @response
+    end  
+
+    def render_response(*args)
+      response(render(*args))
+    end 
+
     def params
       request.params
     end 
